94d009d5ffbe93e220c6b02fd062139026d424d2
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-09-15 15:48:45 +0300

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -73,7 +73,7 @@ export class AuthService {
 
   private async createToken(userId: string) {
     const accessToken = await this.jwt.signAsync({
-      sub: userId,
+      sub: user.id,
     });
 
     return {
