54e815dbf83e401dbdb16c7d2eca4308beb308fc
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-09-15 15:51:44 +0300

    Add JWT authentication to ChatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { WsJwtGuard } from './ws-jwt.guard';
 import { PrismaService } from '../prisma.service';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
+
   controllers: [
     ChatController,
   ],
@@ -13,6 +21,7 @@ import { PrismaService } from '../prisma.service';
   providers: [
     ChatGateway,
     ChatService,
+    WsJwtGuard,
     PrismaService,
   ],
 })
